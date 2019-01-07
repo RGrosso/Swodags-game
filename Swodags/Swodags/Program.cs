bc3b8d7177f6304703190cc0e1249b9caee048ca
Ryan Grosso <37914799+RGrosso@users.noreply.github.com>
2019-01-07 13:21:24 +0000

    Add files via upload

---
+++ Swodags/Swodags/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swodags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Swods[] Party = { new Fire("Fire"), new Water("Water"), new Ice("Ice") };
+            DisplaySwod(Party);
+
+            Swods character = SelectSwod();
+            character.QuickDetails();
+
+            Console.WriteLine("A wild enemy appeared, press any button to continue and see stats.");
+            Console.ReadLine();
+            Swods enemy = new Fire("Enemy");
+            enemy.QuickDetails();
+            Console.WriteLine("attack?");
+            Console.ReadLine();
+            Attack(character,enemy);
+            enemy.QuickDetails();
+        }
+
+        public static void Attack(Swods c ,Swods e)
+        {
+            int damage = (c.Attack / 10);
+            if (c.Special == 0)
+            {
+                Random r = new Random();
+                decimal bonus = (r.Next(10, 20))/10 ;
+                Console.WriteLine(bonus);
+                decimal predamage = (c.Attack) * bonus;
+                Math.Round(predamage, MidpointRounding.AwayFromZero);
+                damage = (int)predamage;
+                Console.WriteLine(damage);
+                Console.WriteLine("You dealed {0} damage!", damage);
+            }
+            e.Health -= damage;
+            Console.WriteLine("Enemies health is now {0}.",e.Health);
+        }
+
+        private static Swods SelectSwod()
+        {
+            Swods res = new Fire("Bob");
+            bool charcreate = false;
+            while (!charcreate)
+            {
+                Console.WriteLine("Please select your class...");
+                Console.WriteLine();
+                string type = Console.ReadLine();
+                res = new Fire("default");
+
+                if (type == "Fire")
+                {
+                    charcreate = true;
+                    Console.WriteLine("You selected Fire");
+                    Console.WriteLine("Choose your characters name...");
+                    string name = Console.ReadLine();
+                    res = new Fire(name);
+                }
+                else if (type == "Water")
+                {
+                    charcreate = true;
+                    Console.WriteLine("You selected Water");
+                    Console.WriteLine("Choose your characters name...");
+                    string name = Console.ReadLine();
+                    res = new Water(name);
+                }
+                else if (type == "Ice")
+                {
+                    charcreate = true;
+                    Console.WriteLine("You selected Ice");
+                    Console.WriteLine("Choose your characters name...");
+                    string name = Console.ReadLine();
+                    res = new Ice(name);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid, try again and make sure you enter the type exactly.");
+                }
+            }
+            return res;
+        }
+
+
+        private static void DisplaySwod(Swods[] Party)
+        {
+            foreach (Swods s in Party)
+            {
+                List<String> details = new List<string>();
+                
+                s.ShowDetails(details);
+                
+                foreach (String x in details)
+                {
+                    
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine("*******************");
+
+
+            }
+        }
+    }
+}
---
+++ Swodags/Swodags/Swods.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swodags
+{
+    public class Swods
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int KillCount { get; set; }
+        public int Special { get; set; }
+        public Swods Type { get; set; }
+
+
+        public Swods(string name)
+        {
+            Name = name;
+            Health = 100;
+            Attack = 100;
+            Defence = 100;
+            KillCount = 0;
+
+        }
+        public virtual List<string> ShowDetails(List<String> result)
+        {
+            result.Add(String.Format("Type:{0}", this.ToString()));
+            result.Add(String.Format("Health:{0}", Health));
+            result.Add(String.Format("Attack:{0}", Attack));
+            result.Add(String.Format("Defence:{0}", Defence));
+            return result;
+        }
+        public virtual void QuickDetails()
+        {
+            Console.WriteLine("******************");
+            Console.WriteLine($"Name:    {Name}");
+            Console.WriteLine($"Type:    {this.ToString()}");
+            Console.WriteLine($"Health:  {Health}");
+            Console.WriteLine($"Attack:  {Attack}");
+            Console.WriteLine($"Defence:{Defence}");
+            Console.WriteLine("******************");
+        }
+        public override string ToString()
+        {
+            return this.GetType().Name;
+        }
+
+    }
+    public class Fire : Swods //attack
+    {
+        public int CriticalBonus { get; set; }
+        public Fire(string name):base(name)
+        {
+            Health = 70;
+            Attack = 150;
+            Special = 0;//Critical bonus damage
+        }
+    }
+    public class Water : Swods //tanky
+    {
+        public int Armour { get; set; }
+        public Water(string name):base(name)
+        {
+            Health = 150;
+            Attack = 70;
+            Armour = 50;
+            Special = 1;//"Heavy armour set"
+        }
+    }
+    public class Ice : Swods //special
+    {
+        public int StatusDamage { get; set; }
+        //public int HealingFactor { get; set; }
+        public Ice(string name):base(name)
+        {
+            StatusDamage = 5;
+            Special = 2;// "Status damage bonus"
+
+        }
+    }
+   
+
+}
